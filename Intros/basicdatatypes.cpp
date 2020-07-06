d1e1a632e7b1defd43ab496b06a5ad742c8826cd
Vardhan Agnihotri <60634457+UltraScienceGuy@users.noreply.github.com>
2020-07-05 19:09:22 -0500

    Create basicdatatypes.cpp

---
+++ Intros/basicdatatypes.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+
+    cin >> a >> b >> c >> d >> e;
+    cout << setprecision(20) << a << endl;
+    cout << setprecision(20) << b << endl;
+    cout << setprecision(20) << c << endl;
+    cout << setprecision(20) << d << endl;
+    cout << setprecision(20) << e << endl;
+
+
+
+    return 0;
+}
