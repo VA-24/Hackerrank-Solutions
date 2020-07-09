bd74d81c950753e9b9c61d8e9c65f7f2599d4d13
Vardhan Agnihotri <60634457+UltraScienceGuy@users.noreply.github.com>
2020-07-09 12:44:51 -0500

    Update VectorSort.cpp

--- STL/VectorSort.cpp
+++ STL/VectorSort.cpp
@@ -1,1 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
